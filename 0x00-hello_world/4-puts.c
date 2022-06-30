838f54b2a811414ee4e6355694ebb48d159580f7
chiomaizuokwu <chigirlangel@yahoo.com>
2022-06-30 13:45:18 -0700

    Task 4.Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 	*main -  this function prints out a text
 	* Returns: 0 if successful
 	*/
-int main - prints a text(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
