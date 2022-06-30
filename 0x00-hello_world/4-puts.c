d7d30c4fbee0154fc61952888cae4b6537cd54e5
chiomaizuokwu <chigirlangel@yahoo.com>
2022-06-30 12:56:08 -0700

    Task 4.Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+	/**
+	*main -  this function prints out a text
+	* Returns: 0 if successful
+	*/
+int main(void)
+{
+	puts("programming is like building a multilingual puzzle\n");
+	return (0);
+}
