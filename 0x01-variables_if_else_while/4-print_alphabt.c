23c22fef9c5da903b71124da9c3155b156d1ae97
chiomaizuokwu <chigirlangel@yahoo.com>
2022-07-01 16:51:54 -0700

    Task 4.Alphabet in lowercase except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Print all the letters in lowercase except q and e
+ * Return: Always (0) success
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x != 'q' && x != 'e')
+			putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
