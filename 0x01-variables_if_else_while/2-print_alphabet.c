8647d8751c05719bd11bf8e847abb33050e28b75
chiomaizuokwu <chigirlangel@yahoo.com>
2022-07-01 15:59:55 -0700

    Task 2.Print alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase
+ * Return: Always (0) success
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
