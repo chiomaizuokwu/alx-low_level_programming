193ed3b6428fa45acad8b297f16cfcc1c9177d64
chiomaizuokwu <chigirlangel@yahoo.com>
2022-07-01 18:23:32 -0700

    Task 9.prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,13 @@ int main(void)
 	int nums;
 
 	for (nums = '0'; nums <= '9'; nums++)
+	{
 		putchar(nums);
 	if (nums != '9')
 	{
 		putchar(',');
 		putchar(' ');
+	}
 	}
 	putchar('\n');
 	return (0);
