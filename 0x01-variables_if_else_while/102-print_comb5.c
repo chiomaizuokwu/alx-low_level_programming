b28d076450ac9707e0ab4b8b78ca3857c159bafb
chiomaizuokwu <chigirlangel@yahoo.com>
2022-07-01 20:35:49 -0700

    Task 12.Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *  main - prints all possible combinations of two two-digit numbers
+ *  Return: Always (0) success
+ */
+int main(void)
+{
+	x1, x2;
+	for (x1 = '0'; x1 <= '98'; x1++)
+	{
+		for (x2 = x1 + 1; x2 <= 99; x2++)
+		{
+			putchar((x1 / 10) + '0');
+			putchar((x1 % 10) + '0');
+			putchar(' ');
+			putchar((x2 / 10) + '0');
+			putchar((x2 % 10) + '0');
+			if (x1 == 98 && x2 == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
