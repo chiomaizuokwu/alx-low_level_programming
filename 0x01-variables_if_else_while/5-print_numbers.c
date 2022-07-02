2a9f0eef6c2416f0622c2d371f79b0ba048a10ef
chiomaizuokwu <chigirlangel@yahoo.com>
2022-07-01 17:04:07 -0700

    Task 5.Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0
+ * Return: Always (0) success
+ */
+int main(void)
+{
+	char number;
+
+	for (number = 0; number < 10; number++)
+		putchar(number);
+	putchar('\n');
+	return (0);
+}
