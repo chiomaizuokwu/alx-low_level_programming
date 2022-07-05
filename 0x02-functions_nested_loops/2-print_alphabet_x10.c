6f5a83e0d2607e0fb706ee119a18294f94a2981c
chiomaizuokwu <chigirlangel@yahoo.com>
2022-07-05 16:40:04 -0700

    Task 2.10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase
+ * Return: Always (0) success
+ */
+void print_alphabet_x10(void)
+{
+	char x, y;
+
+	for (y = 0; y <= 9; y++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n'); :
+	}
+}
+
