9e4a796187416583a6258c9d7f37d185167f6854
chiomaizuokwu <chigirlangel@yahoo.com>
2022-07-14 16:17:50 -0700

    Task 8.Rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * rot13 - Function that encodes a string using rot13.
+ * @str: String to be encoded
+ * Return: A pointer to the encoded string
+ */
+char *rot13(char *str)
+{
+	int x = 0, y;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			     'T', 'U', 'V', 'W', 'X', 'Y',
+			     'Z', 'A', 'B', 'C', 'D', 'E',
+			     'F', 'G', 'H', 'I', 'J', 'K',
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
+
+	while (str[x])
+	{
+		for (y = 0; y < 52; y++)
+		{
+			if (str[x] == alphabe[y])
+			{
+				str[x] = rot13key[y];
+				break;
+			}
+		}
+		x++;
+	}
+
+	return (str);
+}
