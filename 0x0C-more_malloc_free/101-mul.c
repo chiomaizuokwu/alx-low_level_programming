a0918c31fc7e8c1f8cd71d478d6ad74ab184a422
chiomaizuokwu <chigirlangel@yahoo.com>
2022-07-28 16:15:23 -0700

    Task 5.Mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -21,7 +21,7 @@ void _print(char *str, int l)
 			putchar(str[i]);
 		i++;
 	}
-	_putchar('\n');
+	putchar('\n');
 	free(str);
 }
 /**
@@ -111,7 +111,7 @@ int main(int argc, char *argv[])
 	if (argc != 3 || check_for_digits(argv))
 	{
 		for (ti = 0; e[ti]; ti++)
-			_putchar(e[ti]);
+			putchar(e[ti]);
 		exit(98);
 	}
 	for (l1 = 0; argv[1][l1]; l1++)
@@ -123,7 +123,7 @@ int main(int argc, char *argv[])
 	if (a == NULL)
 	{
 		for (ti = 0; e[ti]; ti++)
-			_putchar(e[ti]);
+			putchar(e[ti]);
 		exit(98);
 	}
 	init(a, ln - 1);
@@ -133,7 +133,7 @@ int main(int argc, char *argv[])
 		if (t == NULL)
 		{
 			for (ti = 0; e[ti]; ti++)
-				_putchar(e[ti]);
+				putchar(e[ti]);
 			free(a);
 			exit(98);
 		}
