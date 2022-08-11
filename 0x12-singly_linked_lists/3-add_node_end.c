e49f1a56a075297fca827b4ca0ab24f6c168f975
chiomaizuokwu <chigirlangel@yahoo.com>
2022-08-11 15:56:37 -0700

    Task 3 done

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -20,7 +20,8 @@ list_t *add_node_end(list_t **head, const char *str)
 	for (n = 0; str[n]; n++)
 		;
 
-	new_node->len = n;	new_node->next = NULL;
+	new_node->len = n;
+	new_node->next = NULL;
 	current_node = *head;
 
 	if (current_node == NULL)
