12a8c8ada312f76bc0606c3d43f7251ec917dab7
chiomaizuokwu <chigirlangel@yahoo.com>
2022-10-07 16:21:20 -0700

    Task 2

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/chiomaizuokwu/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
-export LD_PRELOAD=/tmp/iwin.so
+wget -P /tmp/ https://raw.github.com./chiomaizuokwu/alx-low_level_programming/main/0x18-dynamic_libraries/libruin.so
+export LD_PRELOAD=/tmp/libruin.so
